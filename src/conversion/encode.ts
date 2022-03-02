758315d0b75b916f12776c5e809d0ac7516e76d8
Will Byrne <will.alex.byrne@gmail.com>
2022-03-02 15:34:19 +0000

    add eslint

--- src/conversion/encode.ts
+++ src/conversion/encode.ts
@@ -1,6 +1,6 @@
 import { convert } from "@stencila/encoda";
 
-export const convertToHtml = async (journalId: string, articleId: string, toJson: boolean = false): Promise<string> => {
+export const convertToHtml = async (journalId: string, articleId: string, toJson = false): Promise<string> => {
   const converted = await convert(`data/${journalId}/${articleId}/${articleId}.xml`, undefined, { // undefined output here is required to return html, causes console output
     from: 'jats',
     to: toJson ? 'json' : 'html',
