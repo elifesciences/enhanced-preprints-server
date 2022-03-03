61b142edd328b117e5c2541f0339ace0382391d8
Will Byrne <will.alex.byrne@gmail.com>
2022-03-03 11:54:05 +0000

    add subheadings to ToC

--- src/pages/article-page.ts
+++ src/pages/article-page.ts
@@ -1,4 +1,4 @@
-import { JSDOM,  } from 'jsdom';
+import { JSDOM, } from 'jsdom';
 
 export const wrapArticleInHtml = (articleHTML: string): string => {
   const articleFragment = JSDOM.fragment(articleHTML);
@@ -21,10 +21,17 @@ export const wrapArticleInHtml = (articleHTML: string): string => {
       src="https://unpkg.com/@stencila/components@&lt;=1/dist/stencila-components/stencila-components.js"
       type="text/javascript" nomodule=""></script>
       <style>
+        .toc-container {
+          max-width: 69.625rem;
+          margin: auto;
+          padding-left: 48px;
+        }
         .toc-list {
           list-style: none;
-          max-width: 69.625rem;
-          margin: 50px auto auto;
+          padding-left: 0;
+        }
+        .toc-list__subheadings {
+          list-style: none;          
         }
         .toc-list__item {
           min-width: 150px;
@@ -35,15 +42,15 @@ export const wrapArticleInHtml = (articleHTML: string): string => {
           font-weight: bold;
           font-family: Noto Sans,Arial,Helvetica,sans-serif;
         }
+        .toc-list__link--subheading {
+          color: #212121;
+          font-family: Noto Sans,Arial,Helvetica,sans-serif;
+        }
       </style>
   </head>
   <body>
     <main role="main">
-        <ul class="toc-list">
-            ${headings.map(heading => `
-              <li class="toc-list__item"><a class="toc-list__link" href="#${heading.id}">${heading.text}</a></li>
-            `).join('')}        
-        </ul>
+        ${generateToC(headings)}
         ${articleHTML}
     </main>
 </body>
@@ -51,15 +58,50 @@ export const wrapArticleInHtml = (articleHTML: string): string => {
 `;
 };
 
+const generateToC = (headings: Heading[]): string => {
+  return `
+    <div class="toc-container">
+      <h2>Table of Contents</h2>
+      <ul class="toc-list">${headings.map(heading => {
+            const subHeadingListItems = heading.children.map(subHeading => `
+              <li class="toc-list__item"><a class="toc-list__link--subheading" href="#${subHeading.id}">${subHeading.text}</a></li>
+            `);
+            const subHeadings = subHeadingListItems.length ? `<ul class="toc-list__subheadings">${subHeadingListItems.join('')}</ul>` : ''
+            return `
+              <li class="toc-list__item"><a class="toc-list__link" href="#${heading.id}">${heading.text}</a>${subHeadings}</li>
+            `
+          }).join('')
+        }
+      </ul>
+    </div>
+`
+}
+
 const getTitle = (articleDom: DocumentFragment): string => {
   return articleDom.querySelector('h1')?.getAttribute('content') || '';
 }
 
-type Heading = { id: string, text: string };
+type HeadingData = { id: string, text: string };
+type Heading = { children: HeadingData[] } & HeadingData;
 const getHeadings = (articleDom: DocumentFragment): Heading[] => {
-  const headingElements = articleDom.querySelectorAll('article > h2[itemtype="http://schema.stenci.la/Heading"]');
+  const headingElements = articleDom.querySelectorAll('article > [itemtype="http://schema.stenci.la/Heading"]');
+
   return Array.from(headingElements).reduce((headings: Heading[], heading) => {
-    if (heading.textContent) headings.push({ id: heading.getAttribute('id') || '', text: heading.textContent });
+    switch (heading.tagName) {
+      case 'H2':
+        headings.push({
+          id: heading.getAttribute('id') || '',
+          text: heading.textContent || '',
+          children: new Array<HeadingData>()
+        });
+        break;
+      case 'H3':
+        headings[headings.length - 1].children.push({
+          id: heading.getAttribute('id') || '',
+          text: heading.textContent || ''
+        });
+        break;
+    }
     return headings;
   }, new Array<Heading>());
 }
