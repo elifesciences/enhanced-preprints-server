42035f38fa0a97a6c0386e85ff0cef98e8390276
Will Byrne <will.alex.byrne@gmail.com>
2022-03-15 16:35:42 +0000

    mock encoda for tests

--- src/conversion/encode.test.ts
+++ src/conversion/encode.test.ts
@@ -1,12 +1,23 @@
 import { convertJatsToHtml } from "./encode";
+import * as encoda from "@stencila/encoda";
 
 describe('encode', () => {
   describe('JATS XML to HTML', () => {
     it('converts provided JATS XML to HTML', async () => {
-      const html = await convertJatsToHtml('10.1101', '2021.07.05.451181');
+      const convertMock = jest.spyOn(encoda, 'convert');
+      convertMock.mockImplementation(() => Promise.resolve(`<html lang="en">article</html>`));
+      const html = await convertJatsToHtml('journalId', 'articleId');
       expect(html).not.toBeNull();
-      expect(html.startsWith('<article')).toBeTruthy();
-      expect(html.endsWith('</article>')).toBeTruthy();
+      expect(convertMock).toHaveBeenCalledTimes(1);
+      expect(convertMock).toHaveBeenCalledWith('data/journalId/articleId/articleId.xml', undefined, {
+        from: 'jats',
+        to: 'html',
+        encodeOptions: {
+          theme: 'elife',
+          isStandalone: false,
+          isBundle: true,
+        }
+      });
     });
   });
 });
