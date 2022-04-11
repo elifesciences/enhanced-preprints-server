88f57c3413b0f77f8e9bb8a64f140b3f330336f1
Will Byrne <will.alex.byrne@gmail.com>
2022-04-11 11:46:15 +0100

    add body typography to reviews

--- src/reviews/reviews.ts
+++ src/reviews/reviews.ts
@@ -12,7 +12,6 @@ const wrapWithHtml = (reviews: string, doi: string) => `
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://unpkg.com/@stencila/thema@2/dist/themes/elife/styles.css" rel="stylesheet">
     <link rel="stylesheet" href="/styles.css"/>
     <link href="https://api.fonts.coollabs.io/css2?family=Noto+Sans" rel="stylesheet"/>
     <link href="https://api.fonts.coollabs.io/css2?family=Noto+Serif" rel="stylesheet"/>
