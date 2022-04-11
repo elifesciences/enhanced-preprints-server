8d3f5437eb080c517a5075eb2ef230311e155ab4
Will Byrne <will.alex.byrne@gmail.com>
2022-04-11 12:18:44 +0100

    clean up sass for article-list to remove nested element calls

--- src/article-list/article-list.ts
+++ src/article-list/article-list.ts
@@ -8,10 +8,10 @@ export const generateArticleList = (journals: string[], articles: Record<string,
   <body>
     <div class="articles-page">
       <h1 class="articles-page__title">Enhanced Preprint Display</h1>
-      <div class="articles-list">
+      <div class="journals-list">
         ${journals.map(journal => `
         <h2 class="articles-list__heading">${journal}</h2>
-        <ul>
+        <ul class="articles-list">
             ${articles[journal].map(article => `<li><a class="article-list__link" href="/article/${journal}/${article}">${article}</a></li>`)}
         </ul>
         `).join('')}
