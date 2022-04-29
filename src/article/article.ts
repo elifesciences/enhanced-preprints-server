6bfd7c84e5981f226af0a157ce7ea62e80da7f77
Dave Price <d.price@elifesciences.org>
2022-04-29 10:52:24 +0100

    re-adding the default stylesheet from stencila

--- src/article/article.ts
+++ src/article/article.ts
@@ -27,6 +27,7 @@ export const wrapArticleInHtml = (articleHTML: string, doi: string): string => {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://unpkg.com/@stencila/thema@2/dist/themes/elife/styles.css" rel="stylesheet">
     <link href="https://api.fonts.coollabs.io/css2?family=Noto+Sans" rel="stylesheet"/>
     <link href="https://api.fonts.coollabs.io/css2?family=Noto+Serif" rel="stylesheet"/>
       <link rel="stylesheet" href="/styles.css"/>
