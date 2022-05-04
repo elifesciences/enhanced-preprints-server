454294dfe9f2ba9df2535ab4001be0274d7d91ef
Scott Aubrey <scott@aubrey.org.uk>
2022-05-04 15:58:33 +0100

    switch to fetching the docmap from sciety

--- src/reviews/fetch-reviews.ts
+++ src/reviews/fetch-reviews.ts
@@ -6,7 +6,7 @@ type HypothesisResponse = {
 
 type FetchReviews = (doi: string, reviewingGroup: string) => Promise<Array<string>>;
 export const fetchReviews: FetchReviews = async (doi, reviewingGroup) => {
-  const docmaps = fetchDocmaps(doi);
+  const docmaps = await fetchDocmaps(doi);
   const docmap = docmaps.find(docmap => docmap.publisher.id === reviewingGroup);
   if (!docmap) {
     return Promise.reject(`No docmap for reviewingGroup: ${reviewingGroup} and doi: ${doi}`);
@@ -36,9 +36,9 @@ export const fetchReviews: FetchReviews = async (doi, reviewingGroup) => {
   }));
 }
 
-type FetchDocmap = (doi: string) => Array<Docmap>;
-const fetchDocmaps: FetchDocmap = () => {
-  return JSON.parse(hardCodedDocmap);
+type FetchDocmap = (doi: string) => Promise<Array<Docmap>>;
+const fetchDocmaps: FetchDocmap = async (doi) => {
+  return axios(`https://sciety.org/docmaps/v1/articles/${doi}.docmap.json`).then(async (response) => response.data);
 }
 
 type DocmapStep = {
@@ -60,118 +60,3 @@ type Docmap = {
   },
   steps: Record<string, DocmapStep>
 };
-const hardCodedDocmap = `
-[
-  {
-    "@context": "https://w3id.org/docmaps/context.jsonld",
-    "id": "https://sciety.org/docmaps/v1/articles/10.1101/2021.07.05.451181/biophysics-colab.docmap.json",
-    "type": "docmap",
-    "created": "2021-09-08T15:06:52.000Z",
-    "updated": "2021-09-08T15:06:52.000Z",
-    "publisher": {
-      "id": "https://biophysics.sciencecolab.org",
-      "name": "Biophysics Colab",
-      "logo": "https://sciety.org/static/groups/biophysics-colab--4bbf0c12-629b-4bb8-91d6-974f4df8efb2.png",
-      "homepage": "https://biophysics.sciencecolab.org",
-      "account": {
-        "id": "https://sciety.org/groups/biophysics-colab",
-        "service": "https://sciety.org"
-      }
-    },
-    "first-step": "_:b0",
-    "steps": {
-      "_:b0": {
-        "assertions": [],
-        "inputs": [
-          {
-            "doi": "10.1101/2021.07.05.451181",
-            "url": "https://doi.org/10.1101/2021.07.05.451181"
-          }
-        ],
-        "actions": [
-          {
-            "participants": [
-              {
-                "actor": {
-                  "name": "anonymous",
-                  "type": "person"
-                },
-                "role": "peer-reviewer"
-              }
-            ],
-            "outputs": [
-              {
-                "type": "review-article",
-                "published": "2021-09-08T14:51:35.722Z",
-                "content": [
-                  {
-                    "type": "web-page",
-                    "url": "https://hypothes.is/a/Q9GJ9BC0EeyPVBtgAn5Yjw"
-                  },
-                  {
-                    "type": "web-page",
-                    "url": "https://sciety.org/articles/activity/10.1101/2021.07.05.451181#hypothesis:Q9GJ9BC0EeyPVBtgAn5Yjw"
-                  }
-                ]
-              }
-            ]
-          },
-          {
-            "participants": [
-              {
-                "actor": {
-                  "name": "anonymous",
-                  "type": "person"
-                },
-                "role": "peer-reviewer"
-              }
-            ],
-            "outputs": [
-              {
-                "type": "review-article",
-                "published": "2021-09-08T14:28:19.243Z",
-                "content": [
-                  {
-                    "type": "web-page",
-                    "url": "https://hypothes.is/a/A2ZbGBCxEeyu-CsIpygfMQ"
-                  },
-                  {
-                    "type": "web-page",
-                    "url": "https://sciety.org/articles/activity/10.1101/2021.07.05.451181#hypothesis:A2ZbGBCxEeyu-CsIpygfMQ"
-                  }
-                ]
-              }
-            ]
-          },
-          {
-            "participants": [
-              {
-                "actor": {
-                  "name": "anonymous",
-                  "type": "person"
-                },
-                "role": "peer-reviewer"
-              }
-            ],
-            "outputs": [
-              {
-                "type": "review-article",
-                "published": "2021-09-08T14:57:57.652Z",
-                "content": [
-                  {
-                    "type": "web-page",
-                    "url": "https://hypothes.is/a/J2qSChC1EeyvHS8fi9T9oQ"
-                  },
-                  {
-                    "type": "web-page",
-                    "url": "https://sciety.org/articles/activity/10.1101/2021.07.05.451181#hypothesis:J2qSChC1EeyvHS8fi9T9oQ"
-                  }
-                ]
-              }
-            ]
-          }
-        ]
-      }
-    }
-  }
-]`;
