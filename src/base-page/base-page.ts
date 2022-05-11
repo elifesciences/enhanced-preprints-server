20e1be5b1c258b2677cd2508690d710d8dc239a9
Scott Aubrey <scott@aubrey.org.uk>
2022-05-11 08:03:23 +0100

    change to Google font CDN - cool labs isn't working correctly

--- src/base-page/base-page.ts
+++ src/base-page/base-page.ts
@@ -5,8 +5,7 @@ export const basePage = (pageContent: string): string =>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link href="https://api.fonts.coollabs.io/css2?family=Noto+Sans" rel="stylesheet"/>
-        <link href="https://api.fonts.coollabs.io/css2?family=Noto+Serif" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;600&family=Noto+Serif" rel="stylesheet">
         <link rel="stylesheet" href="/styles.css"/>
       </head>
       <body>
