b79d015b8dc7dd78ec1e1cd8a3eb6529593a5f8d
Will Byrne <will.alex.byrne@gmail.com>
2022-05-12 08:58:05 +0100

    remove console.warn

--- src/reviews/fetch-reviews.ts
+++ src/reviews/fetch-reviews.ts
@@ -9,7 +9,6 @@ export const fetchReviews: FetchReviews = async (doi, reviewingGroup) => {
   const docmaps = await fetchDocmaps(doi);
   const docmap = docmaps.find(docmap => docmap.publisher.id === reviewingGroup);
   if (!docmap) {
-    console.warn(`No docmap for reviewingGroup: ${reviewingGroup} and doi: ${doi}`)
     return Promise.resolve([]);
   }
   const hypothesisUrls = Object.values(docmap.steps)
@@ -23,7 +22,6 @@ export const fetchReviews: FetchReviews = async (doi, reviewingGroup) => {
     );
 
   if (!hypothesisUrls.length) {
-    console.warn(`No hypothesis urls found in docmap for reviewingGroup: ${reviewingGroup} and doi: ${doi}`);
     return Promise.resolve([]);
   }
   const hypothesisIds = hypothesisUrls.map(url => {
