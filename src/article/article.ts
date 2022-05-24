dcd3ccfab32f1a12ed98fc39a4a7f4f7a5d516d6
Will Byrne <will.alex.byrne@gmail.com>
2022-05-24 16:40:19 +0100

    add review timelines and generecise right column section styles

--- src/article/article.ts
+++ src/article/article.ts
@@ -10,15 +10,20 @@ export const buildArticlePage = (articleHTML: string, doi: string): string => {
         <div class="article-status">
             <h2 class="article-status__heading">Reviewed Preprint</h2>
             <span class="article-status__text">This preprint has been reviewed by eLife. Authors have responded but not yet submitted a revised edition</span>
-        </div>
-        <div class="article-actions">
-            <a class="article-actions__button" href="#"><span class="material-icons article-actions__button_icon">download</span>Download</a>
-            <a class="article-actions__button" href="#"><span class="material-icons article-actions__button_icon">format_quote</span>Cite</a>
-            <a class="article-actions__button" href="#"><span class="material-icons article-actions__button_icon">notifications</span>Follow</a>
-            <a class="article-actions__button" href="#"><span class="material-icons article-actions__button_icon">share</span>Share</a>
-        </div>
-        <div class="review-link__container">
-          <a class="review-link__anchor" href="/article/${doi}/reviews">Reviews ></a>
+            <div class="article-actions">
+              <a class="article-actions__button" href="#"><span class="material-icons article-actions__button_icon">download</span>Download</a>
+              <a class="article-actions__button" href="#"><span class="material-icons article-actions__button_icon">format_quote</span>Cite</a>
+              <a class="article-actions__button" href="#"><span class="material-icons article-actions__button_icon">notifications</span>Follow</a>
+              <a class="article-actions__button" href="#"><span class="material-icons article-actions__button_icon">share</span>Share</a>
+          </div>
+        </div>        
+        <div class="review-timeline">
+            <ol class="review-timeline__list">
+                <li class="review-timeline__list_item"><span class="review-timeline__event">Author response</span><span class="review-timeline__date">Mar 6, 2022</span></li>
+                <li class="review-timeline__list_item"><span class="review-timeline__event">Peer review</span><span class="review-timeline__date">Mar 3, 2022</span></li>
+                <li class="review-timeline__list_item"><span class="review-timeline__event">Preprint posted</span><span class="review-timeline__date">Nov 8, 2021</span></li>
+            </ol>
+            <a class="review-timeline__reviews_link" href="/article/${doi}/reviews"><span class="material-icons link-icon">arrow_forward</span>Read the peer-review by eLife</a>
         </div>
       </div>
 
