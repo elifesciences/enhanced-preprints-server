1e7f3aa6e042311ff2535125945611cecc187c1e
Scott Aubrey <scott@aubrey.org.uk>
2022-05-31 08:47:34 +0100

    rewrite with the expectation of only one journal

--- src/article-list/article-list.ts
+++ src/article-list/article-list.ts
@@ -1,15 +1,21 @@
 import { ArticleSummary } from "../model/model";
 
-export const generateArticleList = (journalName: string, articleSummaries: ArticleSummary[]): string => `
-  <div class="articles-page">
-    <h1 class="articles-page__title">Enhanced Preprint Display</h1>
-    <div class="journals-list">
-      <h2 class="articles-list__heading">${journalName}</h2>
-      <ul class="articles-list">
-      ${articleSummaries.map(articleSummary => `
-      <li><a class="article-list__link" href="/article/${articleSummary.doi}">${articleSummary.title}</a></li>
-      `).join('')}
-      </ul>
-    </div>
-  </div>
+export const generateArticleList = (journalName: string, articleSummaries: ArticleSummary[]): string => {
+  if (articleSummaries.length == 0) {
+    return wrapInPageHtml(journalName, '<p>No articles found</p>');
+  }
+
+  const articleList = articleSummaries.map(articleSummary => `
+    <li><a class="article-list__link" href="/article/${articleSummary.doi}">${articleSummary.date.toLocaleDateString()} - ${articleSummary.title}</a></li>
+    `).join('');
+  return wrapInPageHtml(journalName, wrapInArticleListHtml(articleList));
+}
+
+const wrapInArticleListHtml = (articleList: string) => `<ul class="articles-list">${articleList}</ul>`;
+
+const wrapInPageHtml = (journalName: string, articleList: string) => `
+<div class="articles-page">
+  <h1 class="articles-page__title">${journalName} Reviewed Preprints</h1>
+  ${articleList}
+</div>
 `;
