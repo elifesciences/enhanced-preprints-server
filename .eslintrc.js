d5993794d91e3465e267e5367eab0718a744d6b1
Will Byrne <will.alex.byrne@gmail.com>
2022-06-07 13:39:44 +0100

    add new linting rules and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,14 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
   ],
+  rules: {
+    'no-multiple-empty-lines': ['warn', { max: 1 }],
+  },
 };
