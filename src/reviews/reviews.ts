763ce6a10472c66a98759f149b4218d51ee400e6
Scott Aubrey <scott@aubrey.org.uk>
2022-06-21 15:50:25 +0100

    start creating a review page layout

--- src/reviews/reviews.ts
+++ src/reviews/reviews.ts
@@ -1,12 +1,21 @@
 import { marked } from 'marked';
 import { EnhancedArticle } from '../model/model';
 
-const wrapWithHtml = (reviews: string, doi: string, noHeader: boolean): string => `<main class="reviews-page" role="main">
-    <a class="return-button" href="/article/${doi}${noHeader ? '?noHeader=true' : ''}"><span class="material-icons return-button__icon">chevron_left</span>Back to article</a>
-    <ul class="review-list">
-        ${reviews}
-    </ul>
-  </main>`;
+const wrapWithHtml = (reviews: string, doi: string, noHeader: boolean): string => `
+  <div class="secondary-column">
+    secondary column
+  </div>
+  <main class="primary-column">
+        <a class="return-button" href="/article/${doi}${noHeader ? '?noHeader=true' : ''}"><span class="material-icons return-button__icon">chevron_left</span>Back to article</a>
+        <div class="table-contents">
+          ToC
+        </div>
+        <div class="main-content-area">
+          <ul class="review-list">
+              ${reviews}
+          </ul>
+        </div>
+      </main>`;
 
 export const generateReviewPage = (article: EnhancedArticle, noHeader: boolean): string => {
   if (article.reviews.length === 0) {
