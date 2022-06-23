bd7ef92a4426f07703adfd1e20432e02afe250ea
nlisgo <nathan@aeopus.com>
2022-06-23 15:08:07 +0100

    Refactor markup

--- src/reviews/reviews-editors-and-reviewers.ts
+++ src/reviews/reviews-editors-and-reviewers.ts
@@ -2,9 +2,9 @@ export const editorsAndReviewers = (): string => `
 <section class="editors-and-reviewers">
   <h2 class="editors-and-reviewers__header" id="editors-and-reviewers">Editors and reviewers</h2>
   <div class="editors-and-reviewers__person">
-    <div class="editors-and-reviewers__person__role">Reviewing Editor</div>
-    <h3 class="editors-and-reviewers__person__name">Eve Marder</h3>
-    <div class="editors-and-reviewers__person__affiliation">Brandeis University, United States</div>
+    <div class="editors-and-reviewers__person-role">Reviewing Editor</div>
+    <div class="editors-and-reviewers__person-name">Eve Marder</div>
+    <div class="editors-and-reviewers__person-affiliation">Brandeis University, United States</div>
   </div>
 </section>
 `;
