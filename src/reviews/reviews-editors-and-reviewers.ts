c4c3dd316a50d3b169f5769a3cf7bab8c713c6c5
nlisgo <nathan@aeopus.com>
2022-06-23 15:13:38 +0100

    Introduce into ul

--- src/reviews/reviews-editors-and-reviewers.ts
+++ src/reviews/reviews-editors-and-reviewers.ts
@@ -1,10 +1,12 @@
 export const editorsAndReviewers = (): string => `
 <section class="editors-and-reviewers">
   <h2 class="editors-and-reviewers__header" id="editors-and-reviewers">Editors and reviewers</h2>
-  <div class="editors-and-reviewers__person">
-    <div class="editors-and-reviewers__person-role">Reviewing Editor</div>
-    <div class="editors-and-reviewers__person-name">Eve Marder</div>
-    <div class="editors-and-reviewers__person-affiliation">Brandeis University, United States</div>
-  </div>
+  <ul class="editors-and-reviewers__list">
+    <li class="editors-and-reviewers__person">
+      <div class="editors-and-reviewers__person-role">Reviewing Editor</div>
+      <div class="editors-and-reviewers__person-name">Eve Marder</div>
+      <div class="editors-and-reviewers__person-affiliation">Brandeis University, United States</div>
+    </li>
+  </ul>
 </section>
 `;
