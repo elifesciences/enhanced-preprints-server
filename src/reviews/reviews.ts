02db023a7603164db8e0d29fee65939d62f8029f
nlisgo <nathan@aeopus.com>
2022-06-24 16:33:57 +0100

    Display peer review of block on reviews page

--- src/reviews/reviews.ts
+++ src/reviews/reviews.ts
@@ -1,13 +1,14 @@
 import { marked } from 'marked';
 import { EnhancedArticle } from '../model/model';
+import { reviewsOf } from './reviews-peer-review-of';
 
-const wrapWithHtml = (reviews: string, doi: string, noHeader: boolean): string => `
+const wrapWithHtml = (reviews: string, article: EnhancedArticle, noHeader: boolean): string => `
   <div class="secondary-column">
-    secondary column
+    ${reviewsOf(article, noHeader)}
   </div>
   <main class="primary-column">
         <div class="table-contents">
-          <a class="return-button" href="/article/${doi}${noHeader ? '?noHeader=true' : ''}"><span class="material-icons return-button__icon">chevron_left</span>Back to article</a>
+          <a class="return-button" href="/article/${article.doi}${noHeader ? '?noHeader=true' : ''}"><span class="material-icons return-button__icon">chevron_left</span>Back to article</a>
         </div>
         <div class="main-content-area">
           <ul class="review-list">
@@ -18,8 +19,8 @@ const wrapWithHtml = (reviews: string, doi: string, noHeader: boolean): string =
 
 export const generateReviewPage = (article: EnhancedArticle, noHeader: boolean): string => {
   if (article.reviews.length === 0) {
-    return wrapWithHtml('<li class="review-list__item"><article class="review-list-content">No reviews found</article></li>', article.doi, noHeader);
+    return wrapWithHtml('<li class="review-list__item"><article class="review-list-content">No reviews found</article></li>', article, noHeader);
   }
   const reviewListItems = article.reviews.map((review) => `<li class="review-list__item"><article class="review-list-content">${marked.parse(review.text)}</article></li>`);
-  return wrapWithHtml(reviewListItems.join(''), article.doi, noHeader);
+  return wrapWithHtml(reviewListItems.join(''), article, noHeader);
 };
