97d824c412181df1fe38039a07e5b33033aeed86
Nathan Lisgo <n.lisgo@elifesciences.org>
2022-07-05 10:32:08 +0100

    Display editors and reviewers on reviews page (#138)

---
+++ src/reviews/reviews-editors-and-reviewers.ts
@@ -0,0 +1,26 @@
+export const editorsAndReviewers = (): string => `
+<section class="editors-and-reviewers">
+  <h2 class="editors-and-reviewers__header" id="editors-and-reviewers">Editors and reviewers</h2>
+  <ul class="editors-and-reviewers__list">
+    <li class="editors-and-reviewers__person">
+      <div class="editors-and-reviewers__person-role">Reviewing Editor</div>
+      <div class="editors-and-reviewers__person-name">Eve Marder</div>
+      <div class="editors-and-reviewers__person-affiliation">Brandeis University, United States</div>
+    </li>
+    <li class="editors-and-reviewers__person">
+      <div class="editors-and-reviewers__person-role">Senior Editor</div>
+      <div class="editors-and-reviewers__person-name">Michael B Eisen</div>
+      <div class="editors-and-reviewers__person-affiliation">University of California, Berkeley, United States</div>
+    </li>
+    <li class="editors-and-reviewers__person">
+      <div class="editors-and-reviewers__person-role">Reviewer 1</div>
+      <div class="editors-and-reviewers__person-name">Jason Pipkin</div>
+      <div class="editors-and-reviewers__person-affiliation">Brandeis University, United States</div>
+    </li>
+    <li class="editors-and-reviewers__person">
+      <div class="editors-and-reviewers__person-role">Reviewer 2</div>
+      <div class="editors-and-reviewers__person-name">Anonymous</div>
+    </li>
+  </ul>
+</section>
+`;
