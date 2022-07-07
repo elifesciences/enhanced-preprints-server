d9739d4f6db6e587cbe1ccfbed1cce035c1eb0bb
Scott Aubrey <scott@aubrey.org.uk>
2022-07-07 07:12:48 +0100

    make alterations to first schema

--- migrations/sqlite/01_create_table_articles.sql
+++ migrations/sqlite/01_create_table_articles.sql
@@ -1,8 +1,9 @@
 CREATE TABLE articles (
     doi TEXT NOT NULL PRIMARY KEY,
-    xml TEXT NOT NULL,
-    html TEXT NOT NULL,
-    json TEXT NOT NULL,
-    title TEXT NOT NULL GENERATED ALWAYS AS (json_extract(json, '$.title')) VIRTUAL,
-    date TEXT NOT NULL GENERATED ALWAYS AS (json_extract(json, '$.datePublished.value')) VIRTUAL
+    title TEXT NOT NULL,
+    date TEXT NOT NULL,
+    authors TEXT NOT NULL,
+    abstract TEXT NOT NULL,
+    licenses TEXT NOT NULL,
+    content TEXT NOT NULL
 );
