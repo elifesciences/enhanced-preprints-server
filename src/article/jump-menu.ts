f3849111b63c31d3e18342af5b90a45d927fd1cc
Scott Aubrey <scott@aubrey.org.uk>
2022-07-08 10:55:57 +0100

    process content in jump menu

--- src/article/jump-menu.ts
+++ src/article/jump-menu.ts
@@ -1,3 +1,4 @@
+import { contentToHtml } from '../model/content';
 import { Heading } from '../model/model';
 
 export const jumpToMenu = (headings: Heading[]): string => (headings.length ? `
@@ -6,7 +7,7 @@ export const jumpToMenu = (headings: Heading[]): string => (headings.length ? `
       <li class="jump-menu-list__item"><a class="jump-menu-list__link" href="#evaluation-summary">eLife review summary</a></li>
       <li class="jump-menu-list__item"><a class="jump-menu-list__link" href="#abstract">Abstract</a></li>
         ${headings.map((heading) => `
-              <li class="jump-menu-list__item"><a class="jump-menu-list__link" href="#${heading.id}">${heading.text}</a></li>
+              <li class="jump-menu-list__item"><a class="jump-menu-list__link" href="#${heading.id}">${contentToHtml(heading.text)}</a></li>
             `).join('')}
       </ul>
     </nav>
