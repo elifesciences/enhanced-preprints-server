1e65f6efc9cb9e51f2cb7e22a9d7b43bbd59e692
Scott Aubrey <scott@aubrey.org.uk>
2022-07-14 17:20:12 +0100

    add an image server, static image endpoint, and a simple image viewer

--- src/article/article-page.ts
+++ src/article/article-page.ts
@@ -11,6 +11,22 @@ export const articlePage = (article: ProcessedArticle, noHeader: boolean): strin
   </div>
   <div class="main-content-area">
     <div class="article-body">
+      <div id="openseadragon1" style="width: 640px; height: 480px;"></div>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/openseadragon/3.1.0/openseadragon.min.js"></script>
+      <script type="text/javascript">
+        var viewer = OpenSeadragon({
+            id: "openseadragon1",
+            preserveViewport: true,
+            visibilityRatio:    1,
+            minZoomLevel:       1,
+            defaultZoomLevel:   1,
+            sequenceMode:       true,
+            prefixUrl: 'https://cdnjs.cloudflare.com/ajax/libs/openseadragon/3.1.0/images/',
+            tileSources: [
+              'http://localhost:8182/iiif/2/test.jpg/info.json'
+            ]
+        });
+      </script>
       ${evaluationSummary(article.doi)}
       ${article.html}
     </div>
