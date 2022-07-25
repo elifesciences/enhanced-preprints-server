ee265f0dbee4ff257c2dd939bd5a120e5e2430de
Scott Aubrey <scott@aubrey.org.uk>
2022-07-25 14:46:10 +0100

    fix: review-of box does not convert title content to html

--- src/reviews/reviews-peer-review-of.ts
+++ src/reviews/reviews-peer-review-of.ts
@@ -1,8 +1,9 @@
+import { contentToHtml } from '../model/content';
 import { EnhancedArticle } from '../model/model';
 
 export const reviewsOf = (article: EnhancedArticle, noHeader: boolean) => `<div class="review-of">
     <h2 class="review-of__heading">Peer review of:</h2>
-    <a class="review-of__article_link" href="/article/${article.doi}${noHeader ? '?noHeader=true' : ''}">${article.title}</a>
+    <a class="review-of__article_link" href="/article/${article.doi}${noHeader ? '?noHeader=true' : ''}">${contentToHtml(article.title)}</a>
     <ol class="review-of__authors">
         <li class="review-of__author">Louis K Scheffer</li>
         <li class="review-of__author">C Shan Xu</li>
