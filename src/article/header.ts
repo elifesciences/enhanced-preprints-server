69a525cdb3385a509a0e3c3f1d35b6b200bd5186
Scott Aubrey <scott@aubrey.org.uk>
2022-08-19 08:44:06 +0100

    fix remaining bugs with content header author and affiliation hiding

--- src/article/header.ts
+++ src/article/header.ts
@@ -4,11 +4,11 @@ import { generateFlags } from './article-flags';
 
 const formatAuthorName = (author: Author) => `${author.givenNames.join(' ')} ${author.familyNames.join('')}`;
 
-const formatOrganisation = (organisation: Organisation) => `${organisation.name}<address class="organisation__address">${organisation.address?.addressCountry ?? ''}</address>`;
+const formatOrganisation = (organisation: Organisation) => `${organisation.name}<address class="content-header-affiliations--affiliation-address">${organisation.address?.addressCountry ?? ''}</address>`;
 
 export const header = (article: ProcessedArticle): string => {
   const organisationList = article.authors.flatMap((author) => author.affiliations).filter((organisation) => !!organisation);
-  const organisationListItems = organisationList.map((organisation: Organisation) => `<li class="organisation">${formatOrganisation(organisation)}</li>`);
+  const organisationListItems = organisationList.map((organisation: Organisation) => `<li class="content-header-affiliations--affiliation">${formatOrganisation(organisation)}</li>`);
   // unique org list
   const uniqueOrganisationListItems = [...new Set(organisationListItems)];
 
@@ -18,16 +18,18 @@ export const header = (article: ProcessedArticle): string => {
     <div class="content-header-authors">
       <input type="checkbox" class="content-header-authors--showall-control" id="content-header-authors--showall-control">
       <ol class="content-header-authors--list">
-        ${article.authors.map((author) => `<li class="person">${formatAuthorName(author)}</li>`).join('')}
+        ${article.authors.map((author) => `<li class="content-header-authors--author">${formatAuthorName(author)}</li>`).join('')}
       </ol>
       <label class="content-header-authors--showall-label" for="content-header-authors--showall-control"></label>
     </div>
 
-    <input type="checkbox" id="content-header__affiliations__showall">
-    <ol class="content-header__affiliations">
-      ${uniqueOrganisationListItems.join('')}
-    </ol>
-    <label for="content-header__affiliations__showall"></label>
+    <div class="content-header-affiliations">
+      <input type="checkbox" class="content-header-affiliations--showall-control" id="content-header-affiliations--showall-control">
+      <ol class="content-header-affiliations--list">
+        ${uniqueOrganisationListItems.join('')}
+      </ol>
+      <label class="content-header-affiliations--showall-label" for="content-header-affiliations--showall-control"></label>
+    </div>
     <div class="content-header__footer">
       <ul class="content-header__identifiers">
         <li class="content-header__identifier"><a href="https://doi.org/${article.doi}">https://doi.org/${article.doi}</a></li>
