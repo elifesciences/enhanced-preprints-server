7fc84d11bbc69767f67230375ddb142c24c8bb9b
Will Byrne <will.alex.byrne@gmail.com>
2023-01-23 11:21:39 +0000

    add JOI to the world

--- src/http-schema/http-schema.test.ts
+++ src/http-schema/http-schema.test.ts
@@ -1,4 +1,4 @@
-import { EnhancedArticleSchema } from './http-schema';
+import { ContentSchema } from './http-schema';
 
 const enhancedArticleExample = {
   id: 'testid1',
@@ -32,63 +32,71 @@ const enhancedArticleExample = {
 };
 
 describe('httpschema', () => {
-  it('coerces dates', () => {
-    const enhancedArticle = EnhancedArticleSchema.parse(enhancedArticleExample);
-    expect(enhancedArticle.article.date).toStrictEqual(new Date('2023-01-02'));
-    expect(enhancedArticle.preprintPosted).toStrictEqual(new Date('2023-01-02'));
-    expect(enhancedArticle.sentForReview).toStrictEqual(new Date('2023-01-03'));
-    expect(enhancedArticle.published).toStrictEqual(new Date('2023-01-23'));
+
+  it('foo', () => {
+    expect(ContentSchema.validate(['multi', 'strings']).error).toStrictEqual(undefined);
+    expect(ContentSchema.validate({ type: 'Paragraph', content: 'PARAGRAPH!!!' }).error).toStrictEqual(undefined);
+    expect(ContentSchema.validate([{ type: 'Paragraph', content: 'PARAGRAPH!!!' }, { type: 'Strong', content: 'STRONG!!!' }]).error).toStrictEqual(undefined);
+    expect(ContentSchema.validate(['This is a ', { type: 'Strong', content: 'STRONG!!!' }, ' string']).error).toStrictEqual(undefined);
+    expect(ContentSchema.validate(['This is a ', { type: 'Strong', content: [{ type: 'Paragraph', content: 'PARAGRAPH!!!' }, 'foo'] }, ' string']).error).toStrictEqual(undefined);
   });
-
-  it('accepts array of strings content', () => {
-    const enhancedArticleExample2 = {
-      ...enhancedArticleExample,
-      article: {
-        ...enhancedArticleExample.article,
-        content: ['multi', 'strings'],
-      },
-    };
-
-    const enhancedArticle = EnhancedArticleSchema.parse(enhancedArticleExample2);
-    expect(enhancedArticle.article.content).toStrictEqual(['multi', 'strings']);
-  });
-
-  it('accepts content object content', () => {
-    const enhancedArticleExample2 = {
-      ...enhancedArticleExample,
-      article: {
-        ...enhancedArticleExample.article,
-        content: { type: 'Heading' },
-      },
-    };
-
-    const enhancedArticle = EnhancedArticleSchema.parse(enhancedArticleExample2);
-    expect(enhancedArticle.article.content).toStrictEqual({ type: 'Heading' });
-  });
-
-  it('accepts array of content object content', () => {
-    const enhancedArticleExample2 = {
-      ...enhancedArticleExample,
-      article: {
-        ...enhancedArticleExample.article,
-        content: [{ type: 'Heading' }, { type: 'Subscript', content: 'Hello' }],
-      },
-    };
-
-    const enhancedArticle = EnhancedArticleSchema.parse(enhancedArticleExample2);
-    expect(enhancedArticle.article.content).toStrictEqual([{ type: 'Heading' }, { type: 'Subscript', content: 'Hello' }]);
-  });
-
-  it('accepts array of mixed strings and content object content', () => {
-    const enhancedArticleExample2 = {
-      ...enhancedArticleExample,
-      article: {
-        ...enhancedArticleExample.article,
-        content: ['This is a ', { type: 'Subscript', content: 'subscripted' }, ' string'],
-      },
-    };
-
-    const enhancedArticle = EnhancedArticleSchema.parse(enhancedArticleExample2);
-    expect(enhancedArticle.article.content).toStrictEqual(['This is a ', { type: 'Subscript', content: 'subscripted' }, ' string']);
-  });
+  // it('coerces dates', () => {
+  //   const enhancedArticle = EnhancedArticleSchema.parse(enhancedArticleExample);
+  //   expect(enhancedArticle.article.date).toStrictEqual(new Date('2023-01-02'));
+  //   expect(enhancedArticle.preprintPosted).toStrictEqual(new Date('2023-01-02'));
+  //   expect(enhancedArticle.sentForReview).toStrictEqual(new Date('2023-01-03'));
+  //   expect(enhancedArticle.published).toStrictEqual(new Date('2023-01-23'));
+  // });
+  //
+  // it('accepts array of strings content', () => {
+  //   const enhancedArticleExample2 = {
+  //     ...enhancedArticleExample,
+  //     article: {
+  //       ...enhancedArticleExample.article,
+  //       content: ['multi', 'strings'],
+  //     },
+  //   };
+  //
+  //   const enhancedArticle = EnhancedArticleSchema.parse(enhancedArticleExample2);
+  //   expect(enhancedArticle.article.content).toStrictEqual(['multi', 'strings']);
+  // });
+  //
+  // it('accepts content object content', () => {
+  //   const enhancedArticleExample2 = {
+  //     ...enhancedArticleExample,
+  //     article: {
+  //       ...enhancedArticleExample.article,
+  //       content: { type: 'Heading' },
+  //     },
+  //   };
+  //
+  //   const enhancedArticle = EnhancedArticleSchema.parse(enhancedArticleExample2);
+  //   expect(enhancedArticle.article.content).toStrictEqual({ type: 'Heading' });
+  // });
+  //
+  // it('accepts array of content object content', () => {
+  //   const enhancedArticleExample2 = {
+  //     ...enhancedArticleExample,
+  //     article: {
+  //       ...enhancedArticleExample.article,
+  //       content: [{ type: 'Heading' }, { type: 'Subscript', content: 'Hello' }],
+  //     },
+  //   };
+  //
+  //   const enhancedArticle = EnhancedArticleSchema.parse(enhancedArticleExample2);
+  //   expect(enhancedArticle.article.content).toStrictEqual([{ type: 'Heading' }, { type: 'Subscript', content: 'Hello' }]);
+  // });
+  //
+  // it('accepts array of mixed strings and content object content', () => {
+  //   const enhancedArticleExample2 = {
+  //     ...enhancedArticleExample,
+  //     article: {
+  //       ...enhancedArticleExample.article,
+  //       content: ['This is a ', { type: 'Subscript', content: 'subscripted' }, ' string'],
+  //     },
+  //   };
+  //
+  //   const enhancedArticle = EnhancedArticleSchema.parse(enhancedArticleExample2);
+  //   expect(enhancedArticle.article.content).toStrictEqual(['This is a ', { type: 'Subscript', content: 'subscripted' }, ' string']);
+  // });
 });
--- src/http-schema/http-schema.ts
+++ src/http-schema/http-schema.ts
@@ -1,128 +1,172 @@
 import { z } from 'zod';
+import Joi from 'joi';
+// import { Content } from '../model/content';
 import { ReviewType } from '../model/model';
 
-const ContentObjectSchema = z.object({
-  type: z.literal('Paragraph')
-    .or(z.literal('Strong'))
-    .or(z.literal('Date'))
-    .or(z.literal('Link'))
-    .or(z.literal('Cite'))
-    .or(z.literal('Heading'))
-    .or(z.literal('Figure'))
-    .or(z.literal('Emphasis'))
-    .or(z.literal('Superscript'))
-    .or(z.literal('Subscript'))
-    .or(z.literal('CiteGroup'))
-    .or(z.literal('ImageObject'))
-    .or(z.literal('Article')),
-}).passthrough();
-const ContentPartSchema = z.string().or(ContentObjectSchema);
-const ContentSchema = ContentPartSchema.or(ContentPartSchema.array());
-
-const ReviewTextSchema = z.string();
-const ParticipantSchema = z.object({
-  name: z.string(),
-  role: z.string(),
-  institution: z.string(),
-});
-const EvaluationSchema = z.object({
-  date: z.coerce.date(),
-  reviewType: z.nativeEnum(ReviewType),
-  text: ReviewTextSchema,
-  participants: ParticipantSchema.array(),
+const ParagraphSchema = Joi.object({
+  type: Joi.string().valid('Paragraph'),
+  content: Joi.link('/'),
 });
 
-const PeerReviewSchema = z.object({
-  evaluationSummary: EvaluationSchema,
-  reviews: EvaluationSchema.array(),
-  authorResponse: EvaluationSchema.optional(),
+const StrongContentSchema = Joi.object({
+  type: Joi.string().valid('Strong'),
+  content: Joi.link('/'),
 });
 
-const AddressSchema = z.object({
-  addressCountry: z.string(),
+const DateContentSchema = Joi.object({
+  type: Joi.string().valid('Date'),
+  content: Joi.link('/'),
 });
 
-const OrganisationSchema = z.object({
-  name: z.string(),
-  address: AddressSchema.optional(),
+const LinkContentSchema = Joi.object({
+  type: Joi.string().valid('Link'),
+  target: Joi.string(),
+  relation: Joi.string().optional(),
+  content: Joi.link('/'),
 });
 
-const IdentifierSchema = z.object({
-  type: z.string(),
-  value: z.string(),
+const CiteContentSchema = Joi.object({
+  type: Joi.string().valid('Cite'),
+  content: Joi.link('/'),
 });
 
-const AuthorSchema = z.object({
-  familyNames: z.string().array(),
-  givenNames: z.string().array(),
-  affiliations: OrganisationSchema.array().optional(),
-  emails: z.string().array().optional(),
-  identifiers: IdentifierSchema.array().optional(),
+const CiteGroupContentSchema = Joi.object({
+  type: Joi.string().valid('CiteGroup'),
+  items: Joi.array().items(CiteContentSchema),
 });
 
-const LicenseSchema = z.object({
-  type: z.string(),
-  url: z.string(),
-});
+const ContentPartSchema = Joi.alternatives().try(
+  Joi.string(),
+  ParagraphSchema,
+  StrongContentSchema,
+  DateContentSchema,
+  LinkContentSchema,
+  CiteContentSchema,
+  CiteGroupContentSchema,
+);
 
-const HeadingSchema = z.object({
-  id: z.string(),
-  text: ContentSchema,
-});
+export const ContentSchema = Joi.alternatives().try(
+  Joi.string(),
+  ContentPartSchema,
+  Joi.array().items(Joi.alternatives().try(Joi.string(), ContentPartSchema)),
+);
 
-const PublicationBaseSchema = z.object({
-  type: z.literal('PublicationVolume').or(z.literal('Periodical')),
-  name: z.string(),
-  volumeNumber: z.number().optional(),
-});
-type Publication = z.infer<typeof PublicationBaseSchema> & {
-  isPartOf?: Publication,
-};
-const PublicationSchema: z.ZodType<Publication> = PublicationBaseSchema.extend({
-  isPartOf: z.lazy(() => PublicationBaseSchema.optional()),
-});
-
-const ReferenceSchema = z.object({
-  type: z.literal('Article'),
-  id: z.string(),
-  title: z.string(),
-  url: z.string(),
-  pageEnd: z.number(),
-  pageStart: z.number(),
-  authors: AuthorSchema.array(),
-  datePublished: z.coerce.date().optional(),
-  isPartOf: PublicationSchema.optional(),
-  identifiers: z.object({
-    type: z.string(),
-    name: z.string(),
-    propertyID: z.string(),
-    value: z.string(),
-  }).array(),
-});
-
-const ProcessedArticleSchema = z.object({
-  doi: z.string(),
-  title: ContentSchema,
-  date: z.coerce.date(),
-  authors: AuthorSchema.array(),
-  abstract: ContentSchema,
-  licenses: LicenseSchema.array(),
-  content: ContentSchema,
-  headings: HeadingSchema.array(),
-  references: ReferenceSchema.array(),
-});
-
-export const EnhancedArticleSchema = z.object({
-  id: z.string(),
-  msid: z.string(),
-  doi: z.string(),
-  versionIdentifier: z.string(),
-  versionDoi: z.string().optional(),
-  article: ProcessedArticleSchema,
-  preprintDoi: z.string(),
-  preprintUrl: z.string(),
-  preprintPosted: z.coerce.date(),
-  sentForReview: z.coerce.date().optional(),
-  peerReview: PeerReviewSchema.optional(),
-  published: z.coerce.date().optional(),
-});
+// const ReviewTextSchema = z.string();
+// const ParticipantSchema = z.object({
+//   name: z.string(),
+//   role: z.string(),
+//   institution: z.string(),
+// });
+// const EvaluationSchema = z.object({
+//   date: z.date(),
+//   reviewType: z.nativeEnum(ReviewType),
+//   text: ReviewTextSchema,
+//   participants: ParticipantSchema.array(),
+// });
+//
+// const PeerReviewSchema = z.object({
+//   evaluationSummary: EvaluationSchema,
+//   reviews: EvaluationSchema.array(),
+//   authorResponse: EvaluationSchema.optional(),
+// });
+//
+// const AddressSchema = z.object({
+//   addressCountry: z.string(),
+// });
+//
+// const OrganisationSchema = z.object({
+//   name: z.string(),
+//   address: AddressSchema.optional(),
+// });
+//
+// const IdentifierSchema = z.object({
+//   type: z.string(),
+//   value: z.string(),
+// });
+//
+// const AuthorSchema = z.object({
+//   familyNames: z.string()
+//     .array(),
+//   givenNames: z.string()
+//     .array(),
+//   affiliations: OrganisationSchema.array()
+//     .optional(),
+//   emails: z.string()
+//     .array()
+//     .optional(),
+//   identifiers: IdentifierSchema.array()
+//     .optional(),
+// });
+//
+// const LicenseSchema = z.object({
+//   type: z.string(),
+//   url: z.string(),
+// });
+//
+// const HeadingSchema = z.object({
+//   id: z.string(),
+//   text: ContentSchema,
+// });
+//
+// const PublicationBaseSchema = z.object({
+//   type: z.literal('PublicationVolume')
+//     .or(z.literal('Periodical')),
+//   name: z.string(),
+//   volumeNumber: z.number()
+//     .optional(),
+// });
+// type Publication = z.infer<typeof PublicationBaseSchema> & {
+//   isPartOf?: Publication,
+// };
+// const PublicationSchema: z.ZodType<Publication> = PublicationBaseSchema.extend({
+//   isPartOf: z.lazy(() => PublicationBaseSchema.optional()),
+// });
+//
+// const ReferenceSchema = z.object({
+//   type: z.literal('Article'),
+//   id: z.string(),
+//   title: z.string(),
+//   url: z.string(),
+//   pageEnd: z.number(),
+//   pageStart: z.number(),
+//   authors: AuthorSchema.array(),
+//   datePublished: z.date().optional(),
+//   isPartOf: PublicationSchema.optional(),
+//   identifiers: z.object({
+//     type: z.string(),
+//     name: z.string(),
+//     propertyID: z.string(),
+//     value: z.string(),
+//   })
+//     .array(),
+// });
+//
+// const ProcessedArticleSchema = z.object({
+//   doi: z.string(),
+//   title: ContentSchema,
+//   date: z.date(),
+//   authors: AuthorSchema.array(),
+//   abstract: ContentSchema,
+//   licenses: LicenseSchema.array(),
+//   content: ContentSchema,
+//   headings: HeadingSchema.array(),
+//   references: ReferenceSchema.array(),
+// });
+//
+// export const EnhancedArticleSchema = z.object({
+//   id: z.string(),
+//   msid: z.string(),
+//   doi: z.string(),
+//   versionIdentifier: z.string(),
+//   versionDoi: z.string()
+//     .optional(),
+//   article: ProcessedArticleSchema,
+//   preprintDoi: z.string(),
+//   preprintUrl: z.string(),
+//   preprintPosted: z.date(),
+//   sentForReview: z.date()
+//     .optional(),
+//   peerReview: PeerReviewSchema.optional(),
+//   published: z.date()
+//     .optional(),
+// });
