b38f49076c41c30aa0437608c88c6d4a5c5d6370
Elliot Jordan Kemp <elliotjordankemp@Elliots-MacBook-Pro.local>
2023-04-04 14:52:41 +0100

    first branch commit

--- src/controller/citations-controller.ts
+++ src/controller/citations-controller.ts
@@ -27,7 +27,36 @@ export const citationsController = () => {
     }
   };
 
+  const downloadRIS = async (req: Request, res: Response, next: NextFunction) => {
+    const {
+      publisherId,
+      articleId,
+    } = req.params;
+    const doi = `${publisherId}/${articleId}`;
+
+    try {
+      const extReq = await axios.get(
+        `https://api.crossref.org/works/${doi}/transform/application/x-research-info-systems`,
+      );
+
+      // TODO: add fallback in case the DOI is not minted yet
+      if (extReq.status !== 404) {
+        const ris = decodeURIComponent(extReq.data);
+
+        if (ris) {
+          res.set({ 'Content-Type': 'application/x-research-info-systems' });
+          res.send(ris);
+        } else {
+          res.status(400);
+        }
+      }
+    } catch (err) {
+      next(err);
+    }
+  };
+
   return {
     downloadBibtex,
+    downloadRIS,
   };
 };
