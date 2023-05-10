5133f11e030c9ad01d82a6562528d20597131818
nlisgo <n.lisgo@elifesciences.org>
2023-05-10 15:15:48 +0100

    Get rid of redundant config REVIEWGROUP_ID and REVIEWGROUP_NAME

--- src/config.ts
+++ src/config.ts
@@ -14,8 +14,6 @@ const getStoreTypeFromString = (repoType: string): StoreType => {
 };
 
 export const config = {
-  id: process.env.REVIEWGROUP_ID ?? 'https://elifesciences.org',
-  name: process.env.REVIEWGROUP_NAME ?? 'eLife',
   repoType: process.env.REPO_TYPE ? getStoreTypeFromString(process.env.REPO_TYPE) : StoreType.InMemory,
   repoConnection: process.env.REPO_CONNECTION ?? './data.db',
   repoUserName: process.env.REPO_USERNAME ?? '',
