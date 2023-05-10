629b1452f245ab2d370a834f325aaeeb4dd20241
Nathan Lisgo <n.lisgo@elifesciences.org>
2023-05-10 15:24:06 +0100

    Remove unused parameter on fetchReviews (#691)

--- src/routes/reviewed-preprints-routes.ts
+++ src/routes/reviewed-preprints-routes.ts
@@ -2,13 +2,13 @@ import express from 'express';
 import { reviewedPreprintsController } from '../controller/reviewed-preprints-controller';
 import { ArticleRepository } from '../model/model';
 
-export const reviewedPreprintsRoutes = (repo: ArticleRepository, config: Record<string, any>) => {
+export const reviewedPreprintsRoutes = (repo: ArticleRepository) => {
   const router = express.Router();
 
-  router.get('/api/reviewed-preprints/', reviewedPreprintsController(repo, config).getReviewedPreprints);
-  router.get('/api/reviewed-preprints/:id(*)/metadata', reviewedPreprintsController(repo, config).getReviewedPreprintMetadata);
-  router.get('/api/reviewed-preprints/:id(*)/content', reviewedPreprintsController(repo, config).getReviewedPreprintContent);
-  router.get('/api/reviewed-preprints/:id(*)/reviews', reviewedPreprintsController(repo, config).getReviewedPreprintReviews);
+  router.get('/api/reviewed-preprints/', reviewedPreprintsController(repo).getReviewedPreprints);
+  router.get('/api/reviewed-preprints/:id(*)/metadata', reviewedPreprintsController(repo).getReviewedPreprintMetadata);
+  router.get('/api/reviewed-preprints/:id(*)/content', reviewedPreprintsController(repo).getReviewedPreprintContent);
+  router.get('/api/reviewed-preprints/:id(*)/reviews', reviewedPreprintsController(repo).getReviewedPreprintReviews);
 
   return router;
 };
