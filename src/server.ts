05bda5c151736b52bcf5a69eaa0ce494d8b5968c
Scott Aubrey <scott@aubrey.org.uk>
2023-09-12 10:41:53 +0100

    Remove in-memory repository (#819)
    * add jest-mongodb and make just enough changes to integrate into the test suite
    
    * fix function define and use out of order
    
    * add missing jest config files
    
    * clean up connections in test suite
    
    * use mongodb connection for integration tests
    
    * correct issue with conflicting aws-sdk types
    
    * fix incorrect logic around the envvars for jest-mongo
    
    * fix up foibles from mongodb
    
    * remove InMemory store
    
    * rename the test suite to reflect it's testing of Mongo repo

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import { ArticleRepository } from './model/model';
-import { createArticleRepository } from './model/create-article-repository';
 import { config } from './config';
 import { logger } from './utils/logger';
 import { createApp } from './app';
+import { createMongoDBArticleRepository } from './model/mongodb/mongodb-repository';
 
 export const server = express();
 
 let articleRepository: ArticleRepository;
-createArticleRepository(config.repoType, config.repoConnection, config.repoUserName, config.repoPassword).then(async (repo: ArticleRepository) => {
+createMongoDBArticleRepository(config.repoConnection, config.repoUserName, config.repoPassword).then(async (repo: ArticleRepository) => {
   articleRepository = repo;
   createApp(articleRepository).listen(config.port, () => {
     logger.info(`EPP server is listening on port ${config.port}`);
