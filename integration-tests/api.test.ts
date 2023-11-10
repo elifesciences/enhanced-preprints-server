7c000e3bb6a1bbe7515fd6f0cd172a74679cf8bd
Scott Aubrey <scott@aubrey.org.uk>
2023-11-10 11:02:22 +0000

    allow a query parameter to enable previews to be seen, and write an integration test for it

--- integration-tests/api.test.ts
+++ integration-tests/api.test.ts
@@ -816,6 +816,89 @@ describe('server tests', () => {
           licenses: [],
           references: [],
         },
+        published: '2023-02-03T00:00:00.000Z',
+      };
+
+      const versionSummary1 = {
+        ...versionSummary,
+        id: 'testid4',
+        versionIdentifier: '1',
+        msid: 'article.2',
+      };
+
+      const versionSummary2 = {
+        id: 'testid5',
+        versionIdentifier: '2',
+        msid: 'article.2',
+        doi: 'test/article.2',
+        preprintDoi: 'preprint/testdoi5',
+        preprintUrl: 'http://preprints.org/preprint/testdoi5',
+        preprintPosted: '2023-02-02T00:00:00.000Z',
+        published: '2023-02-03T00:00:00.000Z',
+      };
+
+      await request(app)
+        .post('/preprints')
+        .send(exampleVersion1)
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200, {
+          result: true,
+          message: 'OK',
+        });
+      await request(app)
+        .post('/preprints')
+        .send(exampleVersion2)
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200, {
+          result: true,
+          message: 'OK',
+        });
+
+      await request(app)
+        .get('/api/preprints/testid4')
+        .expect({
+          article: exampleVersion1,
+          versions: {
+            testid4: versionSummary1,
+            testid5: versionSummary2,
+          },
+        });
+      await request(app)
+        .get('/api/preprints/article.2')
+        .expect({
+          article: exampleVersion2,
+          versions: {
+            testid4: versionSummary1,
+            testid5: versionSummary2,
+          },
+        });
+    });
+
+    it('imports non-published content, which is retrieved using query param `previews`', async () => {
+      const app = createApp(articleStore);
+
+      const exampleVersion1 = {
+        ...enhancedArticle,
+        id: 'testid4',
+        versionIdentifier: '1',
+        msid: 'article.2',
+      };
+      const exampleVersion2 = {
+        id: 'testid5',
+        versionIdentifier: '2',
+        msid: 'article.2',
+        doi: 'test/article.2',
+        preprintDoi: 'preprint/testdoi5',
+        preprintUrl: 'http://preprints.org/preprint/testdoi5',
+        preprintPosted: '2023-02-02T00:00:00.000Z',
+        article: {
+          title: 'Test Article 2',
+          abstract: 'Test article 2 abstract',
+          authors: [],
+          content: '<article></article>',
+          licenses: [],
+          references: [],
+        },
         published: null,
       };
 
@@ -855,16 +938,15 @@ describe('server tests', () => {
         });
 
       await request(app)
-        .get('/api/preprints/testid4')
+        .get('/api/preprints/article.2')
         .expect({
           article: exampleVersion1,
           versions: {
             testid4: versionSummary1,
-            testid5: versionSummary2,
           },
         });
       await request(app)
-        .get('/api/preprints/article.2')
+        .get('/api/preprints/article.2?previews=true')
         .expect({
           article: exampleVersion2,
           versions: {
