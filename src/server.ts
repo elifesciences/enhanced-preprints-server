d0b8296706169b84fbce8920c4e63c82df656319
Scott Aubrey <scott@aubrey.org.uk>
2024-06-03 09:50:02 +0100

    feat: deploy server with otel libraries integrated

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,7 @@
+import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
+import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
 import express from 'express';
 import { ArticleRepository } from './model/model';
 import { config } from './config';
@@ -5,8 +9,18 @@ import { logger } from './utils/logger';
 import { createApp } from './app';
 import { createMongoDBArticleRepository } from './model/mongodb/mongodb-repository';
 
-export const server = express();
+const provider = new NodeTracerProvider();
+provider.register();
 
+registerInstrumentations({
+  instrumentations: [
+    // Express instrumentation expects HTTP layer to be instrumented
+    new HttpInstrumentation(),
+    new ExpressInstrumentation(),
+  ],
+});
+
+export const server = express();
 let articleRepository: ArticleRepository;
 createMongoDBArticleRepository(config.repoConnection, config.repoUserName, config.repoPassword).then(async (repo: ArticleRepository) => {
   articleRepository = repo;
