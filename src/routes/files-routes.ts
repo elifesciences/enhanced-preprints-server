414caf018ba88dbbce160004ed5bbb24b214a470
Scott Aubrey <scott@aubrey.org.uk>
2024-06-19 15:06:56 +0100

    fix non-uniquely addressable route

--- src/routes/files-routes.ts
+++ src/routes/files-routes.ts
@@ -5,7 +5,7 @@ import { ArticleRepository } from '../model/model';
 export const filesRoutes = (repo: ArticleRepository) => {
   const router = express.Router();
 
-  router.get('/api/preprints/:identifier(*)/:fileId(*)', filesController(repo).downloadSupplementaryFile);
+  router.get('/api/files/:identifier(*)/:fileId(*)', filesController(repo).downloadSupplementaryFile);
 
   return router;
 };
