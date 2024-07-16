dcf3971f75de6ff880cdbce160fb360341796e0f
Will Byrne <will.alex.byrne@gmail.com>
2024-07-16 11:52:17 +0100

    add basic first steps [elifesciences/enhanced-preprints-issues#1141]

--- scripts/clone-prod-db.sh
+++ scripts/clone-prod-db.sh
@@ -1,9 +1,14 @@
 # /bin/sh
 
 # port forward to cluster
+kubectl port-forward -n epp--prod service/epp-database-psmdb-db-replicaset 27017:27017
 
 # get creds for db
+password = kubectl get secret epp-database-psmdb-db-secrets -o yaml -n epp--prod | grep 'MONGODB_DATABASE_ADMIN_PASSWORD' | cut -d ':' -f 2 | xargs | base64 -d | cut -d '%' -f 1
 
 # clone db to local file
+mongodump --host 127.0.0.1 --port 27017 --username $(username) --password $(password) --authenticationDatabase admin --db epp --collection versioned_articles --out ./versioned_articles.dump
+
+# kill port forwarding
 
 # push local file to docker container
