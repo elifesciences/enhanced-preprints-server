d3e446db6606b76539add2296e999bf46e018c7a
Will Byrne <will.alex.byrne@gmail.com>
2024-07-16 14:00:07 +0100

    make restore script executable and fix name of local file [elifesciences/enhanced-preprints-issues#1141]

--- scripts/use-prod-db.sh
+++ scripts/use-prod-db.sh
@@ -2,6 +2,6 @@
 
 # push local file to docker container
 mongo_container_id=$(docker ps -f 'name=mongodb' -q)
-docker cp versioned_articles.dump $mongo_container_id:/tmp/staging.dump
+docker cp staging.dump $mongo_container_id:/tmp/staging.dump
 
 docker compose exec mongodb mongorestore --uri="mongodb://admin:testtest@localhost:27017" --authenticationDatabase=admin --drop --nsInclude=epp.versioned_articles /tmp/staging.dump
