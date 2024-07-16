7f0883faa9718ba26b7608879d49cf0ff530e07b
Will Byrne <will.alex.byrne@gmail.com>
2024-07-16 13:46:49 +0100

    update script [elifesciences/enhanced-preprints-issues#1141]

--- scripts/clone-prod-db.sh
+++ scripts/clone-prod-db.sh
@@ -5,9 +5,9 @@ kubectl port-forward -n epp--staging service/epp-database-psmdb-db-replicaset 27
 process_pid=$!
 
 # get creds for db
-credentials = $(kubectl get secret epp-database-psmdb-db-secrets -o yaml -n epp--staging)
-password = $(echo "$credentials" | grep 'MONGODB_DATABASE_ADMIN_PASSWORD' | cut -d ':' -f 2 | xargs | base64 -d | cut -d '%' -f 1)
-username = $(echo "$credentials" | grep 'MONGODB_DATABASE_ADMIN_USERNAME' | cut -d ':' -f 2 | xargs | base64 -d | cut -d '%' -f 1)
+credentials=$(kubectl get secret epp-database-psmdb-db-secrets -o yaml -n epp--staging)
+password=$(echo "$credentials" | grep 'MONGODB_DATABASE_ADMIN_PASSWORD' | cut -d ':' -f 2 | xargs | base64 -d | cut -d '%' -f 1)
+username=$(echo "$credentials" | grep 'MONGODB_DATABASE_ADMIN_USER' | cut -d ':' -f 2 | xargs | base64 -d | cut -d '%' -f 1)
 
 # clone db to local file
 mongodump --host 127.0.0.1 --port 27017 --username $username --password $password --authenticationDatabase admin --db epp --collection versioned_articles --out ./staging.dump
