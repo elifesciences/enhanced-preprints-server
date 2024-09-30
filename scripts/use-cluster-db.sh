b1327ef0f85049e5b813e677e628796ef3f8ce17
Will Byrne <will.alex.byrne@gmail.com>
2024-09-30 14:21:33 +0100

    Database script update (#1256)
    * update the scripts to include a help option and allow the changing of hosts and filenames
    
    * update readme to reflect script changes
    
    * Changes to readme with extra instruction and formatting
    
    * update scripts to use printf for better portability
    
    * update scripts to use printf for better portability
    
    ---------
    
    Co-authored-by: A. K <74007089+soggy-mushroom@users.noreply.github.com>

---
+++ scripts/use-cluster-db.sh
@@ -0,0 +1,36 @@
+# /bin/sh
+
+R="\033[0;31m"
+G="\033[0;32m"
+
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -i|--input)
+      INPUTPATH="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -h|--help)
+      printf "This script will restore a mongodb data dump into a local mongodb docker container."
+      printf
+      printf "options:"
+      printf -e "${G}-i --input      The input path for the database dump including the filename (default: ./database.dump)"
+      exit 0
+      ;;
+
+    -*|--*)
+      printf -e "${R}Unknown option $1"
+      exit 1
+      ;;
+  esac
+done
+
+INPUTPATH="${INPUTPATH:-./database.dump}"
+
+# push local file to docker container
+mongo_container_id=$(docker ps -f 'name=mongodb' -q)
+docker cp ${INPUTPATH} $mongo_container_id:/tmp/database.dump
+
+docker compose exec mongodb mongorestore --uri="mongodb://admin:testtest@localhost:27017" --authenticationDatabase=admin --drop --nsInclude=epp.versioned_articles /tmp/database.dump
