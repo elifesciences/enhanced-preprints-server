30ab8a20ec10efc38c4de3aa12eaf0e5d6065cdf
Scott Aubrey <scott@aubrey.org.uk>
2025-11-19 11:59:37 +0000

    Upgrade wildcard route matching for express v5

--- src/routes/preprints-routes.ts
+++ src/routes/preprints-routes.ts
@@ -8,10 +8,10 @@ export const preprintsRoutes = (repo: ArticleRepository) => {
   const router = express.Router();
 
   router.get('/api/preprints/', preprintsController(repo).getPreprints);
-  router.get('/api/preprints/:identifier(*)', preprintsController(repo).getPreprintsByIdentifier);
+  router.get('/api/preprints/*identifier', preprintsController(repo).getPreprintsByIdentifier);
   router.get('/api/preprints-no-content/', preprintsController(repo).getEnhancedArticlesNoContent);
   router.post<{}, ResponseBodyType, EnhancedArticle>('/preprints', preprintsController(repo).postPreprints);
-  router.delete('/preprints/:identifier(*)', preprintsController(repo).deletePreprintByIdentifier);
+  router.delete('/preprints/*identifier', preprintsController(repo).deletePreprintByIdentifier);
 
   return router;
 };
